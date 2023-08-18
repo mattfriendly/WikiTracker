e4b13a6af83b061e84d996f78012f6a1d53fe646
mattfriendly <dj@mattfriendly.com>
2023-08-17 23:52:36 -0700

    fix: added oauth 2.0 subroutine to script first

--- auth.py
+++ auth.py
@@ -1,57 +1,54 @@
+import os
 import requests
-import dns.resolver
+from requests_oauthlib import OAuth2Session
 
 # Replace with your FedEx API credentials
-api_key = "your_api_key_here"
-account_number = "your_account_number_here"
+client_id = "your_client_id_here"
+client_secret = "your_client_secret_here"
+token_url = "https://apis-sandbox.fedex.com/v1/oauth/token"
+authorization_base_url = "https://apis-sandbox.fedex.com/v1/oauth/authorize"
+tracking_base_url = "https://apis.fedex.com/v1/tracking"
 
-# Base URL for FedEx Tracking API
-base_url = "https://api.e-commerce.fedex.com/v1/tracking"
 
-def resolve_domain(domain):
-    resolver = dns.resolver.Resolver()
-    try:
-        resolved_ips = resolver.resolve(domain)
-        return resolved_ips[0].address
-    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.exception.DNSException):
-        return None
+def authenticate():
+    oauth2_session = OAuth2Session(client_id, redirect_uri="http://localhost")
+    authorization_url, _ = oauth2_session.authorization_url(authorization_base_url)
 
-def get_tracking_info(tracking_number, custom_headers=None, request_data=None):
-    resolved_ip = resolve_domain("apis-sandbox.fedex.com")
+    print("Please go to this URL and grant access:", authorization_url)
+    authorization_code = input("Enter the authorization code from the callback URL: ")
 
-    if not resolved_ip:
-        print("Failed to resolve domain name.")
+    token = oauth2_session.fetch_token(
+        token_url,
+        authorization_response=authorization_code,
+        client_secret=client_secret
+    )
+
+    os.environ["FEDEX_ACCESS_TOKEN"] = token["access_token"]
+    print("Access token stored as environment variable.")
+
+
+def get_tracking_info(tracking_number):
+    access_token = os.environ.get("FEDEX_ACCESS_TOKEN")
+    if not access_token:
+        print("Access token is not available. Please authenticate.")
         return
 
-    # Prepare the default headers with required authentication
     headers = {
-        "Authorization": f"Bearer {api_key}",
-        "account-number": account_number
+        "Authorization": f"Bearer {access_token}",
+        "account-number": "your_account_number_here"
     }
 
-    # If custom headers are provided, update the default headers
-    if custom_headers:
-        headers.update(custom_headers)
+    response = requests.get(f"{tracking_base_url}/{tracking_number}", headers=headers)
 
-    try:
-        # Make a POST request to the resolved IP address using HTTPS
-        url = f"https://{resolved_ip}/v1/tracking"
-        response = requests.post(url, headers=headers, json=request_data)
+    if response.status_code == 200:
+        data = response.json()
+        print("Tracking Information:", data)
+    else:
+        print("API Request failed with status code:", response.status_code)
 
-        if response.status_code == 200:
-            data = response.json()
-            print("Tracking Information:", data)
-        else:
-            print("API Request failed with status code:", response.status_code)
-
-    except requests.exceptions.RequestException as e:
-        print("An error occurred:", e)
 
 if __name__ == "__main__":
     tracking_number = "your_tracking_number_here"
 
-    # Example: Customize headers and request body data
-    custom_headers = {"custom-header": "value"}
-    request_data = {"request_key": "request_value"}
-
-    get_tracking_info(tracking_number, custom_headers, request_data)
+    authenticate()  # Perform OAuth 2.0 authentication
+    get_tracking_info(tracking_number)  # Get tracking information
